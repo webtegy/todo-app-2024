3f426057db6125c2bbc155a2e037896766c01624
Naveendanj <naveenhettiwaththa@gmail.com>
2024-09-11 18:48:20 +0530

    added common button component

--- App.js
+++ App.js
@@ -3,16 +3,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LandingPage from './screens/landing/Landing';
 import BottomTabNavigator from './navigators/BottomTabNavigator';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="LandingPage" component={LandingPage} />
-        <Stack.Screen name="Main" component={BottomTabNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.dark}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="LandingPage" component={LandingPage} />
+          <Stack.Screen name="Main" component={BottomTabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
--- screens/landing/Landing.js
+++ screens/landing/Landing.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View,  StyleSheet , Image } from 'react-native';
+import CommonButton from '../../components/CommonButton';
+
 
 export default function LandingPage({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to the Landing Page</Text>
-      <Button
-        title="Go to Main App"
-        onPress={() => navigation.replace('Main')}
-      />
+    <View style={styles.container}>
+        <Image
+            source={require('../../assets/landing/logo.png')}
+            style={styles.logo}
+            resizeMode="contain"
+        />
+        <View style={styles.buttonContainer}>
+            <CommonButton />
+        </View>
     </View>
   );
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        backgroundColor : '#121212'
+    },
+    logo: {
+        width: 200,
+        height: 200,
+    },
+        buttonContainer: {
+        position: 'absolute',
+        bottom: 50,
+        width: '90%',
+    },
+})
